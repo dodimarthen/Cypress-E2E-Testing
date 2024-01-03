52f73d5b1531f71eef585e7f89a13a0dfaed3a20
marthenss <dodimartin.sit@gmail.com>
2024-01-03 10:42:44 +0700

    first commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,81 @@
+require('Cypress-xpath'),
+
+//LGN_001
+it('Login with valid credential', () =>{
+
+    //visit the page
+    cy.visit('https://courses.ultimateqa.com/users/sign_in');
+
+
+    //fullfill sign-in form with valid credential
+    cy.get('[name="user[email]"]').type("JohnDoeAlpaca20@gmail.com");
+    cy.get('[name="user[password]"]').type("john14045");
+    cy.get('button.button.button-primary.g-recaptcha[data-callback="onSubmit"]').click();
+    cy.get('a[href*="/collections"]')
+    .should('exist');
+})  
+
+//LGN_002
+it('Login with invalid credentials (wrong password)', () =>{
+
+    //visit the page
+    cy.visit('https://courses.ultimateqa.com/users/sign_in');
+
+
+    //fullfill sign-in form with valid credential
+    cy.get('[name="user[email]"]').type("JohnDoeAlpaca20@gmail.com");
+    cy.get('[name="user[password]"]').type("john14046");
+    cy.get('button.button.button-primary.g-recaptcha[data-callback="onSubmit"]').click();
+    cy.wait(25000)
+    cy.get('li.form-error__list-item').contains("Invalid email or password.")
+    .should('exist');
+})  
+
+//LGN_003
+it('Login with invalid credential 2 (wrong email)', () =>{
+
+    //visit the page
+    cy.visit('https://courses.ultimateqa.com/users/sign_in');
+
+
+    //fullfill sign-in form with valid credential
+    cy.get('[name="user[email]"]').type("JohnDoeAlpaca21@gmail.com");
+    cy.get('[name="user[password]"]').type("john14045");
+    cy.get('button.button.button-primary.g-recaptcha[data-callback="onSubmit"]').click();
+    cy.wait(25000)
+    cy.get('li.form-error__list-item').contains("Invalid email or password.")
+    .should('exist');
+})  
+
+//LGN_004
+it('Login with invalid credential 3 (leaves both fields empty)', () =>{
+
+    //visit the page
+    cy.visit('https://courses.ultimateqa.com/users/sign_in');
+
+
+    //fullfill sign-in form with valid credential
+    cy.get('[name="user[email]"]').type(" ");
+    cy.get('[name="user[password]"]').type(" ");
+    cy.get('button.button.button-primary.g-recaptcha[data-callback="onSubmit"]').click();
+    cy.wait(25000)
+    cy.get('li.form-error__list-item').contains("Invalid email or password.")
+    .should('exist');
+})  
+
+//LGN_005
+it('Login with remember me option selected', () =>{
+
+    //visit the page
+    cy.visit('https://courses.ultimateqa.com/users/sign_in');
+
+
+    //fullfill sign-in form with valid credential
+    cy.get('[name="user[email]"]').type("johndoealpaca20@gmail.com");
+    cy.get('[name="user[password]"]').type("john14045");
+    cy.get('.form__label--checkbox').click();
+    cy.get('button.button.button-primary.g-recaptcha[data-callback="onSubmit"]').click();
+    cy.wait(25000)
+    cy.get('a[href*="/collections"]')
+    .should('exist');
+})  
---
+++ cypress/e2e/register.cy.js
@@ -0,0 +1,137 @@
+require('Cypress-xpath'),
+//REG_001
+it('New User Register Successfully', () =>{
+
+    //visit the page
+    cy.visit('https://courses.ultimateqa.com/users/sign_in');
+
+    //verify register or create new account visible
+    cy.get('a[href*="/users/sign_up"]')
+    .should('exist')
+    .click()
+
+    //fullfill registration form with valid credential
+    cy.get('[name="user[first_name]"]').type("John");
+    cy.get('[name="user[last_name]"]').type("Doe");
+    cy.get('[name="user[email]"]').type("JohnDoeAlpaca20@gmail.com");
+    cy.get('[name="user[password]"]').type("john14045");
+    cy.get('[type="checkbox"]').check();
+    cy.get('[data-callback="onSubmit"]').click();
+    cy.get('a[href*="/collections"]')
+    .should('exist');
+})  
+
+//REG_002
+it('Registers a New User with an Email that is Already Taken', () =>{ 
+
+    //visit the page
+    cy.visit('https://courses.ultimateqa.com/users/sign_in');
+
+    //verify register or create new account visible
+    cy.get('a[href*="/users/sign_up"]')
+    .should('exist')
+    .click()
+
+    //fullfill registration form with valid credential
+    cy.get('[name="user[first_name]"]').type("John");
+    cy.get('[name="user[last_name]"]').type("Doe");
+    cy.get('[name="user[email]"]').type("JohnDoeAlpaca20@gmail.com");
+    cy.get('[name="user[password]"]').type("john14045");
+    cy.get('[type="checkbox"]').check();
+    cy.get('[data-callback="onSubmit"]').click();
+    cy.wait(25000)
+    cy.get('li.form-error__list-item').contains('Email has already been taken');
+    
+
+})
+
+//REG_003
+it('Registers a New User with an invalid Email domain', () =>{ 
+
+    //visit the page
+    cy.visit('https://courses.ultimateqa.com/users/sign_in');
+
+    //verify register or create new account visible
+    cy.get('a[href*="/users/sign_up"]')
+    .should('exist')
+    .click()
+
+    //fullfill registration form with valid credential
+    cy.get('[name="user[first_name]"]').type("John");
+    cy.get('[name="user[last_name]"]').type("Doe");
+    cy.get('[name="user[email]"]').type("johndoe01@yopmail.com");
+    cy.get('[name="user[password]"]').type("johndoe123");
+    cy.get('[type="checkbox"]').check();
+    cy.get('[data-callback="onSubmit"]').click();
+
+})
+
+//REG_004
+it('Registers a New User with an Empty FirstName', () =>{ 
+
+    //visit the page
+    cy.visit('https://courses.ultimateqa.com/users/sign_in');
+
+    //verify register or create new account visible
+    cy.get('a[href*="/users/sign_up"]')
+    .should('exist')
+    .click()
+
+    //fullfill registration form with valid credential
+    cy.get('[name="user[first_name]"]').type(" ");
+    cy.get('[name="user[last_name]"]').type("Doe");
+    cy.get('[name="user[email]"]').type("marthensitorus@gmail.com");
+    cy.get('[name="user[password]"]').type("johndoe123");
+    cy.get('[type="checkbox"]').check();
+    cy.get('[data-callback="onSubmit"]').click();
+    cy.wait(30000)
+    cy.get('li.form-error__list-item').contains("First name can't be blank");
+})
+
+//REG_005
+it('Registers a New User with an Empty LastName', () =>{ 
+
+    //visit the page
+    cy.visit('https://courses.ultimateqa.com/users/sign_in');
+
+    //verify register or create new account visible
+    cy.get('a[href*="/users/sign_up"]')
+    .should('exist')
+    .click()
+
+    //fullfill registration form with valid credential
+    cy.get('[name="user[first_name]"]').type("John");
+    cy.get('[name="user[last_name]"]').type(" ");
+    cy.get('[name="user[email]"]').type("marthensitorus@gmail.com");
+    cy.get('[name="user[password]"]').type("johndoe123");
+    cy.get('[type="checkbox"]').check();
+    cy.get('[data-callback="onSubmit"]').click();
+    cy.wait(30000)
+    cy.get('li.form-error__list-item').contains("Last name can't be blank");
+})
+
+//REG_005
+it('Registers a New User with an Empty LastName', () =>{ 
+
+    //visit the page
+    cy.visit('https://courses.ultimateqa.com/users/sign_in');
+
+    //verify register or create new account visible
+    cy.get('a[href*="/users/sign_up"]')
+    .should('exist')
+    .click()
+
+    //fullfill registration form with valid credential
+    cy.get('[name="user[first_name]"]').type(" ");
+    cy.get('[name="user[last_name]"]').type(" ");
+    cy.get('[name="user[email]"]').type(" ");
+    cy.get('[name="user[password]"]').type(" ");
+    cy.get('[type="checkbox"]').check();
+    cy.get('[data-callback="onSubmit"]').click();
+    cy.wait(30000)
+    cy.get('li.form-error__list-item').contains("First name can't be blank");
+    cy.get('li.form-error__list-item').contains("Last name can't be blank");
+    cy.get('li.form-error__list-item').contains("Terms must be accepted");
+    cy.get('li.form-error__list-item').contains("Email can't be blank");
+    cy.get('li.form-error__list-item').contains("Password can't be blank");
+})
